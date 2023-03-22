0200008f7524f0c9e9983bad83789ecf4bc9b387
doo82112 <63440971+doo82112@users.noreply.github.com>
2023-03-22 18:42:01 +0900

    0.5

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const fs = require("fs");
 var cors = require('cors');
 
 
@@ -13,6 +15,22 @@ mongoose
 
 const app = express();
 const port = 3000;
+//이미지 업로드 관련
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'krpano/uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '.jpg');
+    }
+});
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    //console.log(req.file);
+    return res.send(req.file);
+});
+//이미지 업로드 관련
 
 app.use('/', express.static(path.join(__dirname, './krpano'), { index: 'tour.html' }));
 
@@ -27,6 +45,8 @@ app.get('/tourtest', (req, res) => {
     //res.sendFile(path.join(__dirname, '/tour.html'));
 });
 
+
+
 const userRouter = require('./routes/users.js');
 app.use('/users', userRouter);
 
@@ -34,5 +54,9 @@ const xmldataRouter = require('./routes/xmldatas.js');
 app.use('/xmldatas', xmldataRouter);
 
 app.listen(port, () => {
+    const dir = "./uploads";
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
     console.log(`Server listening on port ${port}`);
 });
